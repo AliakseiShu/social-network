1d16b3b2926fb00e652ec05b3c7e0edfdb328a0d
Aliaksei Sulha <aliakseishulha2021@gmail.com>
2022-03-10 13:25:30 +0300

    way 37 start

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,50 @@
-let rerenderEntireTree = () => {
+export let store = {
+  _state: StateType: {
+    profilePage: {
+      newPostText:"",
+      posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: "It's my first post", likesCount: 10},
+      ],
+    },
+    dialogsPage: {
+      dialogs: [
+        {id: 1, name: "Alex"},
+        {id: 2, name: "Zina"},
+        {id: 3, name: "Anna"},
+        {id: 4, name: "Andrey"},
+      ],
+      messages: [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How are You?"},
+        {id: 3, message: "ok"},
+      ]
+    },
+    sidebar: {}
+  },
+ocChange () {
+  console.log('State changed')
+},
+addPost (newPostText: string){
+  const newPost:MyPostsArrayProps={
+    id:5,
+    message: newPostText,
+    likesCount:0
+  }
+  state.profilePage.posts.push(newPost);
+  ocChange()
+},
+updateNewPostText(newText: string){state.profilePage.newPostText=newText
+  ocChange()
+},
+subscribe (callBack: () => void) {
+  ocChange=callBack
+
+}
+}
+
+
+let ocChange = () => {
   console.log('State changed')
 }
 
@@ -37,6 +83,7 @@ export type DialogsPageType = {
 }
 
 
+/*
 export let state: StateType = {
   profilePage: {
     newPostText:"",
@@ -61,6 +108,10 @@ export let state: StateType = {
   sidebar: {}
 }
 
+*/
+
+/*
+
 export let addPost = (newPostText: string)=>{
     const newPost:MyPostsArrayProps={
     id:5,
@@ -68,15 +119,16 @@ export let addPost = (newPostText: string)=>{
     likesCount:0
   }
   state.profilePage.posts.push(newPost);
-  rerenderEntireTree()
+  ocChange()
 }
 export const updateNewPostText = (newText: string)=>{
    state.profilePage.newPostText=newText
-  rerenderEntireTree()
+  ocChange()
 }
 
-export const subscribe = (observer: () => void) => {
-  rerenderEntireTree=observer
+export const subscribe = (callBack: () => void) => {
+  ocChange=callBack
 
 }
 
+*/
