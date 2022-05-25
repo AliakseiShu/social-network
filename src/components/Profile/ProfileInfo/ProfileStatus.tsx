17c6df984e642a761d6408f51c2ae0204dac1bb8
Alex <aliakseishulha2021@gmail.com>
2022-05-25 10:51:28 +0300

    way 73 start - http put, local state

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -18,6 +18,8 @@ class ProfileStatus extends React.Component<ProfileStatusType, ProfileStatusType
         })
     }
     deactivateEditMode = () => {
+        debugger
+        console.log("this",this)
         this.setState({
             editMode: false
         })
@@ -27,12 +29,12 @@ class ProfileStatus extends React.Component<ProfileStatusType, ProfileStatusType
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
+                    <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
-                    <input autoFocus={true} onBlur={ this.deactivateEditMode.bind(this) } value={this.props.status}/>
+                    <input autoFocus={true} onBlur={ this.deactivateEditMode } value={this.props.status}/>
                 </div>
                 }
             </div>
