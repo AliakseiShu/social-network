d692fceb2336d5dda8a60f7e91e81c6f1ac06b49
Alex <aliakseishulha2021@gmail.com>
2022-09-04 21:35:19 +0300

    fixed App.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,6 @@ import SamuraiJSApp from "./App";
 import ReactDOM from 'react-dom';
 import React from "react";
 
-/*test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});*/
-
 test('renders without crashing', () => {
 const div = document.createElement('div')
  ReactDOM.render(<SamuraiJSApp/>, div)
